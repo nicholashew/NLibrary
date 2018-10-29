98621af2b1f92541e370118ee23de2b71156cff7
nicholashew <nicholashew@users.noreply.github.com>
2018-10-29 18:25:53 +0800

    Add test base

---
+++ NLibrary.Tests/TestBase.cs
@@ -0,0 +1,17 @@
+﻿using Xunit.Abstractions;
+
+namespace NLibrary.Tests
+{
+    public abstract class TestBase
+    {
+        /// <summary>
+        /// Gets or sets the logger to use for writing text to be captured in the test results.
+        /// </summary>
+        protected ITestOutputHelper Logger { get; set; }
+
+        protected TestBase(ITestOutputHelper logger)
+        {
+            this.Logger = logger;
+        }
+    }
+}
