5ab411207ef3c2881f259355d96d52178dded331
nicholashew <nicholashew@users.noreply.github.com>
2018-10-31 12:43:44 +0800

    Add encryiption and test

---
+++ NLibrary.Tests/Security/EncryptionTests.cs
@@ -0,0 +1,214 @@
+﻿using NLibrary.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NLibrary.Tests.Security
+{
+    public class EncryptionTests : TestBase
+    {
+        const string DesSecretKey = "secret99";
+        const string TripleDesSecretKey = "secret99asdfghjkqwerty12";
+        const string InitVector = "!@#$%^&*";
+
+        public EncryptionTests(ITestOutputHelper logger)
+            : base(logger)
+        {
+        }
+
+        [Fact]
+        public void Base64Test()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.Base64Encrypt(plainText);
+            string decrypted = Encryption.Base64Decrypt(encrypted);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void MD5Test()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.MD5Hash(plainText);
+
+            Assert.Equal("5eb63bbbe01eeed093cb22bb8f5acdc3", encrypted);
+        }
+
+        [Fact]
+        public void SHA1Test()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.SHA1Hash(plainText);
+
+            Assert.Equal("2AAE6C35C94FCFB415DBE95F408B9CE91EE846ED", encrypted);
+        }
+
+        [Fact]
+        public void DesThrowsInvalidArgumentException()
+        {
+            string plainText = "hello world";
+
+            var tupleList = new List<(string, string)>
+            {
+                ( "secret99", "" ),
+                ( "secret99", "1234567" ),
+                ( "secret99", "1234567890" ),
+                ( "", "!@#$%^&*" ),
+                ( "12345", "!@#$%^&*" ),
+                ( "1234567890", "!@#$%^&*" )
+            };
+
+            foreach (var item in tupleList)
+            {
+                var exception = Record.Exception(() => Encryption.DesEncrypt(plainText, item.Item1, item.Item2));
+                Assert.NotNull(exception);
+                Assert.IsType<ArgumentException>(exception);
+            }
+        }
+
+        [Fact]
+        public void DesTest()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.DesEncrypt(plainText, DesSecretKey, InitVector);
+            string decrypted = Encryption.DesDecrypt(encrypted, DesSecretKey, InitVector);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void DesThrowsInvalidSecretKeyException()
+        {
+            string encrypted = Encryption.DesEncrypt("hello world", DesSecretKey, InitVector);
+
+            var exception = Record.Exception(() => Encryption.DesDecrypt(encrypted, "wrongkey", InitVector));
+            Assert.NotNull(exception);
+            Assert.IsType<System.Security.Cryptography.CryptographicException>(exception);
+        }
+
+        [Fact]
+        public void TripleDESTest()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.TripleDESEncrypt(plainText, TripleDesSecretKey, InitVector);
+            string decrypted = Encryption.TripleDESDecrypt(encrypted, TripleDesSecretKey, InitVector);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void TripleDESThrowsInvalidArgumentException()
+        {
+            string plainText = "hello world";
+
+            var tupleList = new List<(string, string)>
+            {
+                ( "secret99asdfghjkqwerty12", "" ),
+                ( "secret99asdfghjkqwerty12", "1234567" ),
+                ( "secret99asdfghjkqwerty12", "1234567890" ),
+                ( "", "!@#$%^&*" ),
+                ( "12345", "!@#$%^&*" ),
+                ( "1234567890", "!@#$%^&*" )
+            };
+
+            foreach (var item in tupleList)
+            {
+                var exception = Record.Exception(() => Encryption.TripleDESEncrypt(plainText, item.Item1, item.Item2));
+                Assert.NotNull(exception);
+                Assert.IsType<ArgumentException>(exception);
+            }
+        }
+
+        [Fact]
+        public void TripleDESThrowsWeakKeyException()
+        {
+            var exception = Record.Exception(() => Encryption.TripleDESEncrypt("hello world", "secret99secret99secret99", InitVector));
+
+            Assert.NotNull(exception);
+            Assert.IsType<System.Security.Cryptography.CryptographicException>(exception);
+            Assert.Contains("weak key", exception.Message);
+        }
+
+        [Fact]
+        public void TripleDESThrowsInvalidSecretKeyException()
+        {
+            string plainText = "hello world";
+            string encrypted = Encryption.TripleDESEncrypt(plainText, TripleDesSecretKey, InitVector);
+
+            var exception = Record.Exception(() => Encryption.TripleDESDecrypt(encrypted, "!@#$%^&*()1234567890abcd", InitVector));
+            Assert.NotNull(exception);
+            Assert.IsType<System.Security.Cryptography.CryptographicException>(exception);
+        }
+
+        [Fact]
+        public void AES128BitKeyTest()
+        {
+            string plainText = "hello world";
+            string key = "!@#$%^&*()123456";
+            string encrypted = Encryption.AesEncrypt(plainText, key);
+            string decrypted = Encryption.AesDecrypt(encrypted, key);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void AES192BitKeyTest()
+        {
+            string plainText = "hello world";
+            string key = "!@#$%^&*()1234567890qwer";
+            string encrypted = Encryption.AesEncrypt(plainText, key);
+            string decrypted = Encryption.AesDecrypt(encrypted, key);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void AES256BitKeyTest()
+        {
+            string plainText = "hello world";
+            string key = "!@#$%^&*()123456!@#$%^&*()123456";
+            string encrypted = Encryption.AesEncrypt(plainText, key);
+            string decrypted = Encryption.AesDecrypt(encrypted, key);
+
+            Assert.NotEqual(plainText, encrypted);
+            Assert.Equal(plainText, decrypted);
+        }
+
+        [Fact]
+        public void AESThrowsInvalidKeyException()
+        {
+            string plainText = "hello world";
+
+            for (int i = 0; i < 100; i++)
+            {
+                string key = RandomString(i);
+                var exception = Record.Exception(() => Encryption.AesEncrypt(plainText, key));
+                if (i == 16 || i == 24 || i == 32)
+                {
+                    Assert.Null(exception);                 
+                }
+                else
+                {
+                    Assert.NotNull(exception);
+                    Assert.IsType<System.Security.Cryptography.CryptographicException>(exception);
+                }
+            }
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ NLibrary/Security/Encryption.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NLibrary.Security
+{
+    /// <summary>
+    /// Simple Encryption
+    /// </summary>
+    public class Encryption
+    {
+        #region Base64
+
+        // <summary>
+        /// Base64 Encrypts a string value generating a base64-encoded string.
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted</param>
+        /// <returns></returns>
+        public static string Base64Encrypt(string plainText)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(plainText);
+                result = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                result = plainText;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Base64 Decrypts a base64-encoded cipher text value generating a string result.
+        /// </summary>
+        /// <param name="cipherText">Base64-encoded cipher text string to be decrypted.</param>
+        /// <returns></returns>
+        public static string Base64Decrypt(string cipherText)
+        {
+            string result = string.Empty;
+            try
+            {
+                byte[] bytes = Convert.FromBase64String(cipherText);
+                result = Encoding.UTF8.GetString(bytes);
+            }
+            catch
+            {
+                //base 64 chars is null
+                result = cipherText;
+            }
+
+            return result;
+        }
+
+        #endregion Base64
+
+        #region MD5
+
+        /// <summary>
+        /// MD5 Encryption
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted</param>
+        /// <returns></returns>
+        public static string MD5Hash(string plainText)
+        {
+            return MD5Hash(plainText, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// MD5 Encryption
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted</param>
+        /// <param name="encode">encoding to be use</param>
+        /// <returns></returns>
+        public static string MD5Hash(string plainText, Encoding encode)
+        {
+            var hash = new StringBuilder();
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text  
+            md5.ComputeHash(encode.GetBytes(plainText));
+
+            //get hash result after compute it  
+            byte[] result = md5.Hash;
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits  
+                //for each byte  
+                hash.Append(result[i].ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+        #endregion MD5
+
+        #region SHA1
+
+        /// <summary>
+        /// SHA1 Encrypts a string value generating a byte data in hyphenated hexadecimal (base-16) encoded uppercase string.
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted</param>
+        /// <returns>Cipher text with SHA-1 (Secure Hash Algorithm 1)</returns>
+        public static string SHA1Hash(string plainText)
+        {
+            var sha1 = SHA1.Create();
+            string encoded = BitConverter.ToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(plainText))).Replace("-", "");
+            return encoded;
+        }
+
+        #endregion SHA1
+
+        #region DES
+
+        /// <summary>
+        /// DES Encrypts a string value generating a base64-encoded string.
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted</param>
+        /// <param name="secretKey">Secret Key (must be 8 characters long)</param>
+        /// <param name="initVector">Initialization Vector (must be 8 characters long)</param>
+        /// <returns>Cipher text formatted as a base64-encoded string</returns>
+        public static string DesEncrypt(string plainText, string secretKey, string initVector)
+        {
+            if (secretKey.Length != 8)
+                throw new ArgumentException("secretKey should be 8 characters");
+
+            if (initVector.Length != 8)
+                throw new ArgumentException("initVector should be 8 characters");
+
+            byte[] data = Encoding.UTF8.GetBytes(plainText);
+            byte[] result;
+            var DES = new DESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(secretKey),
+                IV = Encoding.UTF8.GetBytes(initVector)
+            };
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+            return Convert.ToBase64String(result);
+        }
+
+        /// <summary>
+        /// DES Decrypts a base64-encoded cipher text value generating a string result.
+        /// </summary>
+        /// <param name="cipherText">Base64-encoded cipher text string to be decrypted.</param>
+        /// <param name="secretKey">Secret Key (must be 8 characters long)</param>
+        /// <param name="initVector">Initialization Vector (must be 8 characters long)</param>
+        /// <returns>Decrypted string value.</returns>
+        public static string DesDecrypt(string cipherText, string secretKey, string initVector)
+        {
+            if (secretKey.Length != 8)
+                throw new ArgumentException("secretKey should be 8 characters");
+
+            if (initVector.Length != 8)
+                throw new ArgumentException("initVector should be 8 characters");
+
+            byte[] data = Convert.FromBase64String(cipherText);
+            var DES = new DESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(secretKey),
+                IV = Encoding.UTF8.GetBytes(initVector)
+            };
+            ICryptoTransform desencrypt = DES.CreateDecryptor();
+            byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);     
+            return Encoding.UTF8.GetString(result);
+        }
+
+        #endregion
+
+        #region 3Des/Triple-DES (Triple Data Encryption Standard)
+
+        /// <summary>
+        /// 3Des Encryption
+        /// </summary>
+        /// <param name="plainText">Plain text string to be encrypted (Internal encoded using UTF8)</param>
+        /// <param name="secretKey">Secret Key (must be 24 characters long)</param>
+        /// <param name="initVector">Initialization Vector (must be 8 characters long)</param>
+        /// <returns>base64 encoded string</returns>
+        public static string TripleDESEncrypt(string plainText, string secretKey, string initVector)
+        {
+            if (secretKey.Length != 24)
+                throw new ArgumentException("secretKey should be 24 characters");
+
+            if (initVector.Length != 8)
+                throw new ArgumentException("initVector should be 8 characters");
+
+            var tdsp = new TripleDESCryptoServiceProvider
+            {
+
+                //Set Initialization Vector
+                IV = Encoding.UTF8.GetBytes(initVector),
+                //Set Secret Key 
+                Key = System.Text.Encoding.UTF8.GetBytes(secretKey),
+                //Set the encryption algorithm for the calculation mode ECB (java compatible)
+                Mode = CipherMode.CBC,
+                //Set the encryption algorithm for the padding mode PKCS7 (java compatible)
+                Padding = PaddingMode.PKCS7
+            };
+
+            //obtain encoded string in utf-8 byte            
+            byte[] data = Encoding.UTF8.GetBytes(plainText);
+
+            //convert into base64 after encrypted
+            ICryptoTransform ct = tdsp.CreateEncryptor();
+            string result = Convert.ToBase64String(ct.TransformFinalBlock(data, 0, data.Length));
+            return result;
+        }
+
+        /// <summary>
+        /// 3Des Decryption
+        /// </summary>
+        /// <param name="cipherText">Base64-encoded cipher text string to be decrypted.</param>
+        /// <param name="secretKey">Secret Key (must be 24 characters long)</param>
+        /// <param name="initVector">Initialization Vector (must be 8 characters long)</param>
+        /// <returns>decrypted string (Internal decoded using UTF8 for decryption)</returns>        
+        public static string TripleDESDecrypt(string cipherText, string secretKey, string initVector)
+        {
+            if (secretKey.Length != 24)
+                throw new ArgumentException("secretKey should be 24 characters");
+
+            if (initVector.Length != 8)
+                throw new ArgumentException("initVector should be 8 characters");
+
+            var tdsp = new TripleDESCryptoServiceProvider
+            {
+                //tdsp.IV = Encoding.UTF8.GetBytes(IV);
+                //Set Initialization Vector
+                IV = Encoding.UTF8.GetBytes(initVector),
+                //Set Secret Key 
+                Key = Encoding.UTF8.GetBytes(secretKey),
+                //Set the encryption algorithm for the calculation mode ECB (java compatible)
+                Mode = CipherMode.CBC,
+                //Set the encryption algorithm for the padding mode PKCS7 (java compatible)
+                Padding = PaddingMode.PKCS7
+            };
+
+            //The encrypted string with base64 encoding, we need to resolve to bytes base64
+            byte[] data = Convert.FromBase64String(cipherText);
+
+            //Decrypt the original string utf8 encoding
+            ICryptoTransform ct = tdsp.CreateDecryptor();
+            string result = Encoding.UTF8.GetString(ct.TransformFinalBlock(data, 0, data.Length));
+            return result;
+        }
+        #endregion 3Des/Triple-DES (Triple Data Encryption Standard)
+
+        #region AES
+
+        /// <summary>
+        ///  AES Encryption
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string AesEncrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
+
+            var rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        ///  AES Decryption
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string AesDecrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            byte[] toEncryptArray = Convert.FromBase64String(str);
+
+            var rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+        #endregion AES
+    }
+}
